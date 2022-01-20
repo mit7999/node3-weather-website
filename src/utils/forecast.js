b541ba157bdc11d84396b6e35f3bab44fc3914fa
mit7999 <mitpandya1990@gmail.com>
2022-01-20 16:50:10 +0530

    longitude latitude bugs solved

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const forecast = (latitide, longitude, callback)=> {
-    const url = 'http://api.weatherstack.com/current?access_key=95e6ff3fb373a4989809801022ab06f8&query=' + longitude +','+ latitide + '&units=f'
+    const url = 'http://api.weatherstack.com/current?access_key=95e6ff3fb373a4989809801022ab06f8&query=' + latitide +','+ longitude 
 
     request({url, json: true}, (error, {body}) =>{
         if(error) {
--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const geocode = (address, callback) =>{
-    const url ='https://api.mapbox.com/geocoding/v5/mapbox.places/'+ encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoibWl0Nzk5OSIsImEiOiJja3lpZmx5OWkyYzNrMnZwYmxnamI2aG8wIn0.Thon87ueexGrYOPOf1sSUg'
+    const url ='https://api.mapbox.com/geocoding/v5/mapbox.places/'+ encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoibWl0Nzk5OSIsImEiOiJja3lpZmx5OWkyYzNrMnZwYmxnamI2aG8wIn0.Thon87ueexGrYOPOf1sSUg&limit=1'
     request({url, json: true},(error, {body}) =>{
         if (error){
             callback('unable to connect to location services' , undefined)
